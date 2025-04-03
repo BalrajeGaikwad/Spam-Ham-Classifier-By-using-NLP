6d869232447abeb394092ab2e1a3abde2f4baa29
Balraje Dattatray Gaikwad <104307925+BalrajeGaikwad@users.noreply.github.com>
2025-04-03 18:53:39 +0530

    By Using TF-IDF And Lemmatization
    Accuracy Score is 97.77

---
+++ spam_ham_classifier_by_using_NLP.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  3 18:33:53 2025
+
+@author: Admin
+"""
+
+import pandas as pd
+
+message = pd.read_csv(
+    r'C:\Users\Admin\Downloads\sms+spam+collection\SMSSpamCollection',
+    sep='\t',
+    names=['label', 'message']
+)
+
+import nltk 
+import re
+
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+wordnet=WordNetLemmatizer()
+corpus=[]
+
+for i in range(0, len(message)):
+    review= re.sub('[^a-zA-Z]', ' ', message['message'][i])
+    review=review.lower()
+    review=review.split()
+    review=[wordnet.lemmatize(word) for word in review if not word in stopwords.words('english')]
+    review=" ".join(review)
+    corpus.append(review)    
+    
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+cv=TfidfVectorizer(max_features=5000)
+x=cv.fit_transform(corpus).toarray()
+
+y=pd.get_dummies(message['label'],dtype=int)
+y=y.iloc[:,1].values
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y , test_size=0.25, random_state=0)
+
+from sklearn.naive_bayes import MultinomialNB
+spam_dataset_model=MultinomialNB().fit(x_train,y_train)
+y_pred=spam_dataset_model.predict(x_test)
+
+##y_pred
+
+from sklearn.metrics import confusion_matrix
+confusion_m=confusion_matrix(y_test,y_pred)
+
+from sklearn.metrics import accuracy_score
+score=accuracy_score(y_test, y_pred)
+
+
+#### Accuracy score is = 97.77
